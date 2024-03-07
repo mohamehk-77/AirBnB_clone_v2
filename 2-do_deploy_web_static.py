0f2829d2b93943d7d8342e8e957402dd23a6ee15
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-03-07 19:34:45 +0200

    File

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,26 +10,21 @@ env.hosts = ['100.26.173.131', '54.90.9.164']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers"""
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
+        F_N = archive_path.split("/")[-1]
+        N_Ext = file_n.split(".")[0]
+        File_Path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        File_Base = archive_path.split('/')[1]
-        File_Name = File_Base.split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}/'.format(File_Name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(File_Base, File_Name))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/\
-            releases/{}/'.format(File_Name, File_Name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(File_Name))
-        run('rm /tmp/{}'.format(File_Base))
+        run('mkdir -p {}{}/'.format(File_Path, N_Ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(F_N, File_Path, N_Ext))
+        run('rm /tmp/{}'.format(F_N))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(File_Path, N_Ext))
+        run('rm -rf {}{}/web_static'.format(File_Path, N_Ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'
-            .format(File_Name))
-
-        print("New version deployed!")
+        run('ln -s {}{}/ /data/web_static/current'.format(File_Path, N_Ext))
         return True
     except:
         return False
