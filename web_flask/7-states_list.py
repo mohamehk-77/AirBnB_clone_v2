fbe5aa85942ba952d6bb36aecde9830fa14093cc
Mohamed kamal <kmido9729@gmail.com>
2024-03-25 00:01:50 +0200

    webflask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application.
+starts a Flask web application.
 """
 
 from models import storage
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False  # Allow URLs with and without trailing slashes
+app.url_map.strict_slashes = False
 
 
 @app.route("/states_list")
 def states_list():
-    """
-    Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name (ascending order).
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
     """
     states = storage.all("State")
-    sorted_states = sorted(
-        states.values(), key=lambda state: state.name)  # Sort by name
-    return render_template("7-states_list.html", states=sorted_states)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """
-    Removes the current SQLAlchemy session after each request.
-    """
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)  # Specify port explicitly
+    app.run(host="0.0.0.0")
